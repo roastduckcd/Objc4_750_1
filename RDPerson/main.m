b48507fcee49ef0d8c90e44a8f7319f02e834c4c
roastduckcd <roart_duck@163.com>
2019-03-04 14:17:27 +0800

    debugable objc4.750.1

---
+++ RDPerson/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RDPerson
+//
+//  Created by yang song on 2019/3/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
